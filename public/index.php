e86207a670fe49d0d4fc195d795b6fa36e83e08f
Oussama KBAILI <ussama9baili@gmail.com>
2025-12-30 13:44:56 +0100

    Fix paths in public/index.php

--- public/index.php
+++ public/index.php
@@ -5,13 +5,13 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/../CDDAM/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require __DIR__.'/../CDDAM/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
-$app = require_once __DIR__.'/../CDDAM/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
