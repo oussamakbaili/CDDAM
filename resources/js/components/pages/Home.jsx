26989367effe828a78565b0858f35cfbdc7cb7aa
Oussama KBAILI <ussama9baili@gmail.com>
2026-01-19 14:43:38 +0100

    RÃ©organisation section Mot du Fondateur: image en haut, texte en bas

--- resources/js/components/pages/Home.jsx
+++ resources/js/components/pages/Home.jsx
@@ -130,9 +130,22 @@ function Home() {
             {/* Mot du Président Section */}
             <section className="president-message-section py-5">
                 <div className="container">
-                    <div className="row g-5 align-items-center">
-                        {/* Contenu du Message */}
-                        <div className="col-lg-8 col-md-7 order-lg-1 order-2">
+                    <div className="row">
+                        {/* Image du Président - En haut */}
+                        <div className="col-12 mb-5">
+                            <div className="president-image-wrapper">
+                                <div className="president-image-glow"></div>
+                                <img
+                                    src="/images/president.jpeg"
+                                    alt={`${t('home.presidentName')} - ${t('home.presidentRole')}`}
+                                    className="president-image"
+                                />
+                                <div className="president-image-overlay"></div>
+                            </div>
+                        </div>
+
+                        {/* Contenu du Message - En bas */}
+                        <div className="col-12">
                             <div className="president-message-content">
                                 <div className="president-message-header">
                                     <h2 className="president-message-title">
@@ -208,19 +221,6 @@ function Home() {
                                         <p className="president-role">{t('home.presidentRole')}</p>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-
-                        {/* Image du Président */}
-                        <div className="col-lg-4 col-md-5 order-lg-2 order-1">
-                            <div className="president-image-wrapper">
-                                <div className="president-image-glow"></div>
-                                <img
-                                    src="/images/president.jpeg"
-                                    alt={`${t('home.presidentName')} - ${t('home.presidentRole')}`}
-                                    className="president-image"
-                                />
-                                <div className="president-image-overlay"></div>
                             </div>
                         </div>
                     </div>
